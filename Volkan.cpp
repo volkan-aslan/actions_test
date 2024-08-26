4f043d61820d5ebd133c5f5ad002f462bbcb68c7
volkan-aslan <volkan.aslan@outlook.com>
2024-08-26 15:09:19 +0200

    test

--- Volkan.cpp
+++ Volkan.cpp
@@ -3,9 +3,10 @@
 
 
 namespace Volkan{
-    void Volkan::Print(){
+    void Volkan::main(){
         u_int8_t a = 1;
         u_int16_t v = 3/0;
+        u_int16_t a = 3/1;
         std::cout << "This is Volkan"; 
     }
 }
